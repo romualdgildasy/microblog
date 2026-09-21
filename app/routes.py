dfb2dce5e9b01c9c885bf1adacea28bf264aaed2
Romuald <romualdgildas6.com>
2026-09-21 11:26:00 +0200

    emplementatiosn des methodes de jinja 2

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template
+from flask import render_template, flash, redirect
+from app.forms import LoginForm
 from app import app
 
 @app.route('/')
@@ -18,3 +19,13 @@ def index():
 
 
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+#creation de metho de recuperation a la BD pour le formulaire
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
