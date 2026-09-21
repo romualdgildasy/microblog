21a47ce71f9feb1187849c3c7ebf5db8959d8aba
Romuald <romualdgildas6.com>
2026-09-21 18:56:04 +0200

    création des formulairees de connexions et registration et les methode de validation de formulaire

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 #appel de la methode de configuration 
@@ -11,5 +12,8 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 #initialisation de la migration de la base de données
 migrate = Migrate(app, db)
+#initialisation de la gestion des utilisateurs
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes,models
--- app/models.py
+++ app/models.py
@@ -1,10 +1,13 @@
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
+from flask_login import UserMixin
+from app import login
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
                                                 unique=True)
@@ -18,6 +21,12 @@ class User(db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     body: so.Mapped[str] = so.mapped_column(sa.String(140))
@@ -30,3 +39,8 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+#récupérer un utilisateur spécifique dans la base de données à partir de son identifiant
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
